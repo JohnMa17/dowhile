62199f35fbd356b4f7d7e9a0935802c94e55c290
ag <ag@DESKTOP-4BN28S3>
2024-06-28 15:31:36 -0400

    Agregar archivos de proyecto.

---
+++ dowhile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nombre, apellido;
+        bool continuar;
+
+        do
+        {
+            Console.WriteLine("Ingrese su nombre: \n");
+            nombre = Console.ReadLine().Trim();
+
+
+            Console.WriteLine("Ingrese su apellido: \n");
+            apellido = Console.ReadLine().Trim();
+
+
+            Console.WriteLine($"Hola, {nombre} {apellido}!");
+
+
+            Console.WriteLine(" Desea ingresar otro nombre y apellido? (s/n)");
+            string respuesta = Console.ReadLine().ToLower();
+
+
+            continuar = (respuesta == "s");
+
+
+        } while ( continuar );
+
+
+        Console.WriteLine("Gracias por usar este programa. Presione cualquier tecla para salir");
+        Console.ReadKey();
+    }
+}
